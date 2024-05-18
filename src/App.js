623d5c76b9281a44ac86d184cc2f7dd3ce0deab0
Ajay Reddy <ajayreddy.karedla@gmail.com>
2024-05-18 16:06:25 +0530

    added some extra data in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>This is my E-Commerce App</h1>
+      <h1>This is my E-Commerce app</h1>
+      <h1>This is my E-Commerce app</h1>
+      <h1>This is my E-Commerce app</h1>
     </div>
   );
 }
